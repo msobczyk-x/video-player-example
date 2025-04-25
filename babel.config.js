c5a9232e26707cd96b5f392f3ce78c5517e75ecd
msobczyk-x <m.sobczyk55@wp.pl>
2025-04-26 00:46:19 +0200

    Initialized httpClient instance, added fonts, changed icons/splashscreen, added assets

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
 module.exports = (api) => {
   api.cache(true);
-
   return {
     presets: ["babel-preset-expo"],
-
     plugins: [["react-native-unistyles/plugin"]],
   };
 };
---
+++ lib/httpClient.ts
@@ -0,0 +1,5 @@
+import ky from "ky";
+
+const httpClient = ky.create({});
+
+export default httpClient;
